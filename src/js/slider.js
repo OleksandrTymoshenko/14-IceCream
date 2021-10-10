2002ebfc0fde1eadb6305ce907cb35ae0223d389
Artur Kravchenko <arturkra92@gmail.com>
2021-10-11 02:56:51 +0300

    Improove styles and fix HTML

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('.slider').slick({
         arrows: false,
         dots: true,
-        // adaptiveHeight: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1000,
