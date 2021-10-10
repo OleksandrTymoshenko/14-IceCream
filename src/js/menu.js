272b1b19850684aa4bab13b7d9a5a25dde6301e4
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2021-10-10 23:47:22 +0300

    добавил header

--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-(() => { const refs = { openMenuBtn: document.querySelector('.menu-open'), closeMenuBtn:
-document.querySelector('.menu-close'), menu: document.querySelector('.mob-menu'), };
-
-refs.openMenuBtn.addEventListener('click', toggleMenu);
-refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-    function toggleMenu() { refs.menu.classList.toggle('is-hidden'); }
-})();
