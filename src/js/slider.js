e9c57db96dc78754a538e9df563782d98e3542d0
Artur Kravchenko <arturkra92@gmail.com>
2021-10-10 19:31:51 +0300

    Add slider styles and html

---
+++ src/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        // adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        veriavleWidth: true
+    });
+});
