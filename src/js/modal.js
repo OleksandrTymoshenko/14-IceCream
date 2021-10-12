b71170622ee3510c4ebbee06e5a17555390bbc2f
OleksandrTymoshenko <nomakievip@gmail.com>
2021-10-12 17:41:31 +0300

    bug fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.forEach(btn =>{
+    btn.addEventListener('click', toggleModal);
+  })
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
