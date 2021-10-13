67705d5b069ae2ea6fcf0fdd4be8b895856ffa72
AndriyBerezovskyi <dyus95@gmail.com>
2021-10-13 18:52:36 +0300

    body scroll fixed, up-button fixed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,17 +2,20 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const btnContainer = document.querySelector('.button-container');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle('is-open');
+    btnContainer.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
