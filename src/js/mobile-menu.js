0a25d8714d350682720e4d4363b1d497ae37c688
OleksandrTymoshenko <87830543+OleksandrTymoshenko@users.noreply.github.com>
2021-10-13 19:10:19 +0300

    Revert "body scroll fixed, up-button fixed"

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,8 +1,6 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-  const btnContainer = document.querySelector('.button-container');
   const closeMenuBtn = document.querySelectorAll('.js-close-menu');
 
   const toggleMenu = () => {
@@ -10,13 +8,11 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    document.body.classList.toggle('is-open');
-    btnContainer.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
